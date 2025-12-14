12fa0cdb3360439ac750c5ddbf81406c3c4879de
liamthura <168834227+liamthura@users.noreply.github.com>
2025-12-14 16:40:05 +0000

    Add GOOGLE_MAPS_API_KEY to configuration

--- backend/app/config.py
+++ backend/app/config.py
@@ -24,6 +24,9 @@ class Settings(BaseSettings):
     
     # App
     DEBUG: bool = True
+
+    # Google Maps
+    GOOGLE_MAPS_API_KEY: Optional[str] = None
     
     class Config:
         env_file = ".env"
